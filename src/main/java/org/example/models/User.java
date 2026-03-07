bc06301e6036c59db80019dc32533aa25a0217e9
Ahmed Mostafa <ahmedmost4444@gmail.com>
2026-03-07 19:16:16 +0200

    movie and user classes

--- src/main/java/org/example/models/User.java
+++ src/main/java/org/example/models/User.java
@@ -1,4 +1,28 @@
 package org.example.models;
 
+import java.util.List;
+
 public class User {
+
+    private String username;
+    private String userId;
+    private List<String> likedCategories;
+
+    public User(String username, String userId, List<String> likedCategories) {
+        this.username = username;
+        this.userId = userId;
+        this.likedCategories = likedCategories;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public List<String> getLikedCategories() {
+        return likedCategories;
+    }
 }
--- src/main/java/org/example/output/RecommendationWriter.java
+++ src/main/java/org/example/output/RecommendationWriter.java
@@ -1,4 +1,33 @@
 package org.example.output;
 
-public class RecommendationWriter {
+import org.example.models.Movie;
+import org.example.models.User;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/*
+public class Recommendation {
+
+    private User user;
+    private Map<String, List<Movie>> categoryMovies;
+
+    public Recommendation(User user) {
+        this.user = user;
+        this.categoryMovies = new HashMap<>();
+    }
+
+    public void addMovies(String category, List<Movie> movies) {
+        categoryMovies.put(category, movies);
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Map<String, List<Movie>> getCategoryMovies() {
+        return categoryMovies;
+    }
 }
+*/
