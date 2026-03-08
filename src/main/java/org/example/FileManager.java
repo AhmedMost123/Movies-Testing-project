83624e6a238e9fba703aa15d78208bd3eb8bc59f
Mariam Hany <mar.hany@hotmail.com>
2026-03-08 21:29:36 +0200

    Adding file manager

--- src/main/java/org/example/FileManager.java
+++ src/main/java/org/example/FileManager.java
@@ -1,33 +1,76 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class FileManager {
 
-    public static List<Map<String, Object>> readFile(String path) {
-        /*
-            if file couldnt be found or access rejected or any other issue occur, return null.
-            returned value should be in the following format:
-            [
-            {
-             "id": "<id1 value>",
-             "label": "<label1 value>",
-             "category": List(["<category1>", "<category2>", ....])
-            },
-            {
-             "id": "<id2 value>",
-             "label": "<label2 value>",
-             "category": List(["<category1>", "<category2>", ....])
-            },
-            ....]
-        */ 
+    public static List<Map<String, Object>> readFile(String path) 
+    {
 
-        return null;
+        List<Map<String, Object>> data = new ArrayList<>();
+        try 
+        {
+            BufferedReader reader = new BufferedReader(new FileReader(path));
+            String line1;
+            String line2;
+
+            //each item consists of two lines
+            while ((line1 = reader.readLine()) != null) 
+            {
+
+                line2 = reader.readLine(); // categories line
+                if (line2 == null) 
+                {
+                    break;
+                }
+
+                //split first line at the comma (label,id)
+                String[] firstParts = line1.split(",");
+                String label = firstParts[0].trim();
+                String id = firstParts[1].trim();
+
+                //split categories at the comma
+                String[] cats = line2.split(",");
+                List<String> categories = new ArrayList<>();
+
+                for (String c : cats) 
+                {
+                    categories.add(c.trim());
+                }
+
+                // create map
+                Map<String, Object> item = new HashMap<>();
+                item.put("label", label);
+                item.put("id", id);
+                item.put("category", categories);
+                data.add(item);
+            }
+
+            reader.close();
+            return data;
+
+        } 
+        catch (Exception e) 
+        {
+            return null;
+        }
     }
 
     public static void writeFile(String path, String content) {
 
-    }
+        try {
+            FileWriter writer = new FileWriter(path);
+            writer.write(content);
+            writer.close();
+        } catch (Exception e) {
+            System.out.println("Error writing file.");
+        }
 
+    }
 }
