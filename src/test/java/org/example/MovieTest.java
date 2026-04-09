57e60f28b176c88d33680fabddcd0af05f0f52ff
amr-elmowaled <33426691+amr-elmowaled@users.noreply.github.com>
2026-04-09 17:22:54 +0200

    implementing unit test for movie ID validation method, and merged the two movie title validation tests into one

--- src/test/java/org/example/MovieTest.java
+++ src/test/java/org/example/MovieTest.java
@@ -2,6 +2,8 @@ package org.example;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -10,7 +12,7 @@ public class MovieTest {
     private static char[] alphabet = new char[26];
 
     @BeforeAll
-    public static void initializeTest() {
+    private static void initializeTest() {
         for (char c = 'a'; c <= 'z'; c++) {
             alphabet[c - 'a'] = c;
         }
@@ -21,8 +23,43 @@ public class MovieTest {
 
     }
 
+    /*
+    ID: Must consist of all capital letters from the movie title followed by three unique digits.
+    testing for: 
+      - ID consisting of all capital letters from the movie title
+      - followed by exactly 3 digits
+    */
     @Test
     void testIsValidMovieID() {
+
+        String[][] testCasesPositive = {
+            {"Man Of Steel", "MOS312"},
+            {"Oppenheimer", "O999"},
+            {"Assassins Creed", "CA864"}, // no letter ordering constraint
+            {"Vikings", "V007"}
+        }; 
+
+        for(String[] testData : testCasesPositive) {
+            assertTrue(new Movie(testData[0], testData[1], null).isValidMovieID()
+            , "failed at case:" + Arrays.toString(testData));
+        }
+
+        String[][] testCasesNegative = {
+            {null, "IRGC314"},
+            {"Man Of Steel", "MOK312"}, // K not in title, and S is missing
+            {"Tenet", "T1234"},
+            {"Banana Republic", "BR00"},
+            {"KING KONG", "KK659"}, // "ALL capital letters of the movie title, not just the first letter of each word"
+            {"Time Traveler", "319TT"},
+            {"The Underworld", "$@318"},
+            {"La Casa De Papel", "LCDP#@!"},
+            {"Jumanji", "JK419"}, // J is here, but K is not in title.
+        };
+
+        for(String[] testData : testCasesNegative) {
+            assertFalse(new Movie(testData[0], testData[1], null).isValidMovieID()
+            , "failed at case:" + Arrays.toString(testData));
+        }
 
     }
 
@@ -30,11 +67,9 @@ public class MovieTest {
      * Every word must start with a capital letter.
     */
     @Test
-    void testIsValidMovieTitle_Positive() { 
-        // Positive test
-       
-
-        String[] testCases = {
+    void testIsValidMovieTitle() { 
+        
+        String[] testCasesPositive = {
             "Tenet ", 
             "Nodirbek Abdussatarov",
             "Brave       Heart",
@@ -43,7 +78,7 @@ public class MovieTest {
             " The Lord Of The Rings"
         };
 
-        for(String title : testCases) {
+        for(String title : testCasesPositive) {
             assertTrue(new Movie(title, null, null).isValidMovieTitle(),
              "failed at case:" + title);
         }
@@ -57,18 +92,8 @@ public class MovieTest {
         assertTrue(new Movie(finalTest, null, null).isValidMovieTitle(),
              "failed at case:" + finalTest);
 
-    }
 
-
-    /*
-     * Every word must start with a capital letter.
-    */
-    @Test
-    void testIsValidMovieTitle_Negative() { 
-        // Negative test
-       
-
-        String[] testCases = {
+        String[] testCasesNegative = {
             "wonderland", 
             "Spiderman 2",
             "Breaking $Bad",
@@ -77,7 +102,7 @@ public class MovieTest {
             
         };
 
-        for(String title : testCases) {
+        for(String title : testCasesNegative) {
             assertFalse(new Movie(title, null, null).isValidMovieTitle(),
              "failed at case:" + title);
         }
